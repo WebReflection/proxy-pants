e0332a5aa2fef410c49c88e94a7edfafbd32085d
Andrea Giammarchi <andrea.giammarchi@gmail.com>
2021-12-09 07:30:31 +0100

    Using Reflect.get instead of target[key]

--- cjs/bound.js
+++ cjs/bound.js
@@ -1,10 +1,11 @@
 'use strict';
 const {Proxy} = require('./proxy.js');
-const {bind} = require('./function.js');
+const {bind, get} = require('./function.js');
 
 const handler = {
-  get: (target, name) => bind(target[name], target)
+  get(target, name) {
+    return bind(get(target, name), target);
+  }
 };
-
 const bound = target => new Proxy(target, handler);
 exports.bound = bound;
--- esm/bound.js
+++ esm/bound.js
@@ -1,8 +1,9 @@
 import {Proxy} from './proxy.js';
-import {bind} from './function.js';
+import {bind, get} from './function.js';
 
 const handler = {
-  get: (target, name) => bind(target[name], target)
+  get(target, name) {
+    return bind(get(target, name), target);
+  }
 };
-
 export const bound = target => new Proxy(target, handler);
