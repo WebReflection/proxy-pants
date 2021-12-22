87228ec62090fb62fd3869398f6268876e981f04
Andrea Giammarchi <andrea.giammarchi@gmail.com>
2021-12-22 09:13:31 +0100

    Faster species access for secured classes

--- cjs/secure.js
+++ cjs/secure.js
@@ -11,18 +11,16 @@ const {species} = Symbol;
 const handler = {
   get(target, name) {
     const Native = target[name];
-    class Secure extends Native {
-      static get [species]() {
-        return Secure;
-      }
-    }
+    class Secure extends Native {}
+
     const proto = getOwnPropertyDescriptors(Native.prototype);
     delete proto.constructor;
     freeze(defineProperties(Secure.prototype, proto));
+
     const statics = getOwnPropertyDescriptors(Native);
     delete statics.length;
     delete statics.prototype;
-    delete statics[species];
+    statics[species] = {value: Secure};
     return freeze(defineProperties(Secure, statics));
   }
 };
